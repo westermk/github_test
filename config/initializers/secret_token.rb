d5829ffa09e395681d148e8123b5ddaac35384f1
westermk <westermk@iupui.edu>
2014-03-15 08:31:57 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubTest::Application.config.secret_key_base = '3299fda238f539385eeb7e2abebc73238ee48429897ab1f5ebf44bbe0803b61fdfabff1ea7e4f43cf0b5d9debe98b238ee242f80938bf31aa36285b6916d6a6e'
